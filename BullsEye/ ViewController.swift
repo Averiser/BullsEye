22c5eddb1a874fc581d8ec878348cb7b0f861c32
MyMacBook <Averiser>
2019-01-10 21:49:03 +0300

    1st commit 31

---
+++ BullsEye/ ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by MyMacBook on 09/01/2019.
+//  Copyright © 2019 MyMacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    @IBOutlet weak var slider: UISlider!
+    var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    var score = 0
+    @IBOutlet weak var scoreLabel: UILabel!
+    var round = 0
+    @IBOutlet weak var roundLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        startNewRound()
+    }
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewRound() {
+        round += 1 // round = round + 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func showAlert() {
+        
+        let difference: Int = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points // score = score + points
+        
+        let message = "You scored \(points) points"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+}
+
